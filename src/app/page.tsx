3926c478356b0174da6458c14a1795b9d30a57c7
Prateek Yadav <prateek246729@gmail.com>
2026-04-28 10:34:50 +0530

    change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,9 @@
 import { About } from "@/components/about"
 import { Achievements } from "@/components/achievements"
 import Contact from "@/components/contact"
-import { Experience } from "@/components/experience"
 import { Footer } from "@/components/footer"
 import { Hero } from "@/components/hero"
 import { Navbar } from "@/components/navbar"
-import { Projects } from "@/components/projects"
-import { Skills } from "@/components/skills"
 
 export default function Page() {
   return (
@@ -16,9 +13,9 @@ export default function Page() {
       <Navbar />
       <Hero />
       <About />
-      <Projects />
+      {/* <Projects />
       <Experience />
-      <Skills />
+      <Skills /> */}
       <Achievements />
       <Contact />
       <Footer />
